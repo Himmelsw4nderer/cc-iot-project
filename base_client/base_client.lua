d6c821ee52aa0654e8bfdf431746358596acbff8
Nils <nils@nbaxde>
2024-12-30 19:41:09 +0100

    Verbessere die Verbindungslogik des Basis-Clients und passe Methoden zur Verwendung von Instanzvariablen an

--- base_client/base_client.lua
+++ base_client/base_client.lua
@@ -6,17 +6,16 @@ function base_client:new(o, modemSide, brokerID)
     setmetatable(o, self)
     self.__index = self
     self.brokerID = brokerID or nil
-    o:connect(modemSide, brokerID)
+    self.modemSide = modemSide or "back"
     return o
 end
 
 -- Funktion zum Verbinden mit dem Broker
-function base_client.connect(modemSide, brokerID)
-    rednet.open(modemSide) -- Netzwerkmodem an der "right"-Seite
+function base_client:connect()
+    rednet.open(self.modemSide)
     rednet.broadcast({ command = "CONNECT" })
     local senderID, message = rednet.receive()
-    if message and message.status == "connected" and senderID == brokerID then
-        self.brokerID = senderID -- Save the broker ID in the object
+    if message and message.status == "connected" and senderID == self.brokerID then
         print("Mit Broker verbunden. Broker ID: " .. self.brokerID)
         return true
     else
@@ -26,18 +25,18 @@ function base_client.connect(modemSide, brokerID)
 end
 
 -- Funktion zum Abonnieren eines Topics
-function base_client.subscribe(topic)
+function base_client:subscribe(topic)
     if not self.brokerID then
         error("Keine Verbindung zu einem Broker. Bitte zuerst verbinden!")
     end
 
-    rednet.send(brokerID, { command = "SUBSCRIBE", topic = topic })
+    rednet.send(self.brokerID, { command = "SUBSCRIBE", topic = topic })
     print("Abonniert: " .. topic)
 end
 
 -- Funktion zum Publizieren auf ein Topic
-function base_client.publish(topic, data)
-    rednet.send(brokerID, { command = "PUBLISH", topic = topic, data = data })
+function base_client:publish(topic, data)
+    rednet.send(self.brokerID, { command = "PUBLISH", topic = topic, data = data })
     print("Publiziert: " .. topic .. " -> " .. data)
 end
 
